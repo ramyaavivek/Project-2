129eb548ea5ed4b71a773fffe3f0320f3d42aca8
80s Action Hero <alex.stanley89@gmail.com>
2018-12-21 19:22:14 -0800

    Radar Chart shows up

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -68,3 +68,86 @@ d3.json("/gender").then(function(d){
     mcount.innerHTML=data[2];
     ocount.innerHTML=data[3];
 });
+
+///////////////////////////
+// Radar Chart
+
+// get data using d3
+d3.json('/codinglanguages').then(d => {
+    // log data for verificatiion
+    console.log('Coding Language Data');
+    console.log(d);
+
+    // Need select devtype from user entry. For now, hard code it
+    var ds = 'Data scientist or machine learning specialist';
+    var backend = 'Back-end developer';
+
+    console.log(d[ds]);
+
+    var dev1= d[ds]
+    var dev2 = d[backend]
+
+    // use object.enteries and forEach to iterate over each dev data and push the keys to labels list and values to data list
+    // define function to format data
+    function dataFormatter(ob1, ob2) {
+        // define empty lists to hold data and labels
+        var data1 = [];
+        var labels = [];
+        var data2 = [];
+
+        // loop over the input object and push the values to data list and keys to labels list
+        Object.entries(ob1).forEach(([key, value]) => {
+            labels.push(key);
+            data1.push(value);
+        });
+
+        // labels are the same for all data
+        Object.entries(ob2).forEach(([key, value]) => {
+            data2.push(value);
+        });
+
+        // dictionary to hold all the lists of data
+        var dataDict = {
+                        data1: data1,
+                        data2: data2,
+                        labels: labels,
+                        };
+
+        return dataDict;
+    }
+
+    var langData = {};
+    
+    langData = dataFormatter(dev1, dev2);
+
+    console.log(langData.data1, langData.data2, langData.labels);
+
+    // All data contained in data object. Let's build the chart!
+    var ctxRadar = document.getElementById('radar').getContext('2d');
+
+    var options = {
+        scale: {
+            // Hides the scale
+            // display: false
+        }
+    };
+
+    var myRadarChart = new Chart(ctxRadar, {
+        type: 'radar',
+        data: {
+            labels: langData.labels,
+            datasets: [
+                {
+                    data: langData.data1,
+                    backgroundColor: 'blue'
+                }, 
+                {
+                    data: langData.data2,
+                    backgroundColor: 'orange'
+                },
+            ]
+        },
+        options: options
+    });
+
+})
