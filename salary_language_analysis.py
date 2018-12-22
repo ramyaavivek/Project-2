803a7c5b47485026afe6b3e3da4e9c8190e95d08
80s Action Hero <alex.stanley89@gmail.com>
2018-12-22 06:14:48 -0800

    changed salary-language calculation

--- salary_language_analysis.py
+++ salary_language_analysis.py
@@ -64,16 +64,38 @@ def salaryLanguageAnalysis():
     for language in topLang:
         
         # dynamically create new column headers
-        print('Checking for: ' + language + '...')        
+        print('Checking for: ' + language + '...')  
+
+        cName = 'salary'+language      
         
         # Create new column to check if the respondent knows a language. If the languege is in the list of known languages, the salary of that respondent, else it gets 0
-        expdev_df[language] = np.where(expdev_df.LanguageList.apply(lambda x: language in x), expdev_df['ConvertedSalary'], False)
+        expdev_df[language] = np.where(expdev_df.LanguageList.apply(lambda x: language in x), 1, 0)
+        expdev_df[cName] = np.where(expdev_df.LanguageList.apply(lambda x: language in x), expdev_df['ConvertedSalary'], 0)
         
         print(f'{language} done!')
 
-    # Aggregation by mean gives the average salary for that language
-    salLangDev = expdev_df.groupby('DevType').mean()
-    salLangDev = salLangDev.drop(index='', columns=['Respondent', 'ConvertedSalary'])
+    # Aggregate by sum. This will get both the number of coders for a language but also the total salary for that language
+    totals = expdev_df.groupby('DevType').sum()
+    # drop unnecessary colums
+    totals = totals.drop(columns=['Respondent', 'ConvertedSalary'], index='')
+
+    counts = totals[topLang]
+
+    # empty list to hold column names
+    salaryList = []
+
+    # concat salery to each language so it matches the column names like in the prev for loop
+    for l in topLang:
+        c = 'salary' + l
+        salaryList.append(c)
+
+    sals = totals[salaryList]
+
+    # rename colums to their original
+    sals.columns = topLang
+
+    # divide salaries by count. Thsi oonly takes into accuont people whoo said they knew the language
+    salLangDev = sals/counts
 
     # Round each percent to 2 decimals
     salLangDev = salLangDev.round(decimals=2)
